89a27c470c1f2bc003d1d23d333af762031af80f
ChimaBright <blakechima@gmail.com>
2025-10-25 10:39:29 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main className='max-w-7xl mx-auto'>
+            <Navbar />
+        </main>
+    )
+}
+export default App
