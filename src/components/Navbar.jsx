feeae1e6007318dd87edd1c81bf4a6da4fa7bf8b
ChimaBright <blakechima@gmail.com>
2025-10-27 15:46:45 +0100

    navbar completion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,23 @@
 import React, {useState} from 'react'
 import {navLinks} from "../constants/index.js";
 
-const Nav_items = () => {
-    return (
-        <ul className='nav-ul'>
-            {navLinks.map(({ id, href, name}) => (
-                 <li key={id} className='nav-li'>
-                    <a href={href} className='nav-li_a' onClick={() => {}}>{name}</a>
-                 </li>
-            ))}
-        </ul>
-    )
-}
+const Nav_Items = ({ onClick = () => {} }) => (
+    <ul className="nav-ul">
+        {navLinks.map((item) => (
+            <li key={item.id} className="nav-li">
+                <a href={item.href} className="nav-li_a" onClick={onClick}>
+                    {item.name}
+                </a>
+            </li>
+        ))}
+    </ul>
+);
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false);
 
-    const toggleMenu = () => {
-        setIsOpen((prevIsOpen) => !prevIsOpen)
-    }
     return (
         <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
             <div className='max-w-7xl mx-auto'>
@@ -35,9 +34,16 @@ const Navbar = () => {
                     </button>
 
                     <nav className='sm:flex hidden'>
-                        <Nav_items />
+                        <Nav_Items />
                     </nav>
+
                 </div>
+            </div>
+
+            <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+                <nav className="p-5">
+                    <Nav_Items onclick={closeMenu}/>
+                </nav>
             </div>
         </header>
     )
